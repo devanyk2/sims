2e4164b3006575a0581b71868593a205e898ef65
devanyk2 <devanyk2@wwu.edu>
2020-05-10 18:59:28 -0700

    Added Game of life, will need to debug

---
+++ life/lifeDef.h
@@ -0,0 +1,15 @@
+/*created by Kieran Devany 
+ * Definitions and global variables
+ */
+
+/*Protos*/
+
+
+/* Globals */
+
+#ifndef EXTERN
+#define EXTERN extern 
+#endif
+
+EXTERN int LENGTH;
+EXTERN int WIDTH;
